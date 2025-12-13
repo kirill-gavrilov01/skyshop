9b922979f4ea199bd42a8511fa8b97fb8e6e8037
homework <kirill_gavrilov01@bk.ru>
2025-12-13 20:29:15 +1000

    3

--- skyshop/src/main/java/org/skypro/skyshop/SkyshopApplication.java
+++ skyshop/src/main/java/org/skypro/skyshop/SkyshopApplication.java
@@ -1,13 +1,10 @@
 package org.skypro.skyshop;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class SkyshopApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(SkyshopApplication.class, args);
 	}
-
 }
